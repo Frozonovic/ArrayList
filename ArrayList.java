2c512b4485c5282630ccd93eb11eecadb44107bb
glacialfrozone <AzD9?tu3>
2022-10-08 18:39:29 -0700

    Is this my first commit? Absolutely. Did I spend a lot of time on this to make sure I did it properly? Absolutely. Enjoy.

--- ArrayList.java
+++ ArrayList.java
@@ -1,3 +1,268 @@
-public class ArrayList<E> {
-    // TODO implement class
+/**
+ * A custom version ArrayList class modeled after default ArrayList class
+ *
+ * @param <E>
+ */
+public class ArrayList<E>
+{
+    // Constants
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final int DEFAULT_RETURN = -1;
+    private static final int LOW_BOUND = 0;
+    private static final int ITERATION = 1;
+    private static final int DOUBLE = 2;
+
+
+
+    // Instance Variables
+    private int _size = 0;
+    private int _capacity;
+    private E[] _backingArray;
+
+
+    // Constructors
+    /**
+     * Creates an instance of class ArrayList object with capacity of 10
+     */
+    public ArrayList()
+    {
+        this(DEFAULT_CAPACITY);
+    }
+
+
+    /**
+     * Creates an instance of class ArrayList object
+     *
+     * @param initialCapacity Starting capacity for new ArrayList instance
+     * @throws IllegalArgumentException Throws if initialCapacity input is invalid
+     */
+    @SuppressWarnings("unchecked")
+    public ArrayList(int initialCapacity) throws IllegalArgumentException
+    {
+        if (initialCapacity < LOW_BOUND)
+        {
+            throw new IllegalArgumentException("Error: Initial capacity must be greater than or equal to 0");
+        }
+        else if (initialCapacity == LOW_BOUND)
+        {
+            _capacity = DEFAULT_CAPACITY;
+            _backingArray = (E[]) new Object[DEFAULT_CAPACITY];
+        }
+
+        else
+        {
+            _capacity = initialCapacity;
+            _backingArray = (E[]) new Object[initialCapacity];
+        }
+    }
+
+
+    // Instance Methods
+    /**
+     * Inserts an element at a designated index and shifts anything accordingly
+     *
+     * @param index Index to insert element at
+     * @param element Element to insert into backing array
+     * @throws IndexOutOfBoundsException Throws error if index is not within backing array's range
+     */
+    public void add(int index, E element) throws IndexOutOfBoundsException
+    {
+        if (index < LOW_BOUND || index > _size)
+        {
+            throw new IndexOutOfBoundsException("Error: Must provide a valid index");
+        }
+
+        else if (index == _size)
+        {
+            add(element);
+        }
+
+        else
+        {
+            grow();
+
+            for (int i = _size; i > index; i--)
+            {
+                E elementToAdd = get(i - ITERATION);
+                _backingArray[i] = elementToAdd;
+            }
+
+            _backingArray[index] = element;
+            _size += ITERATION;
+        }
+    }
+
+
+    /**
+     * Appends an element to the backing array
+     *
+     * @param element Element to append to array
+     * @return Boolean value based on success of method
+     */
+    public boolean add(E element)
+    {
+        grow();
+        _backingArray[_size] = element;
+        _size += ITERATION;
+        return true;
+    }
+
+
+    /**
+     * Clears the backing array by nulling out each value and resets size to 0
+     */
+    public void clear()
+    {
+        for (int i = LOW_BOUND; i < _size; i++)
+        {
+            add(i, null);
+        }
+
+        _size = LOW_BOUND;
+    }
+
+
+    /**
+     * Fetches the element at a given index
+     *
+     * @param index Index of element to fetch
+     * @return The element at the given index
+     * @throws IndexOutOfBoundsException Throws error if index is not within backing array's range
+     */
+    public E get(int index) throws IndexOutOfBoundsException
+    {
+        if (index < LOW_BOUND || index >= _size)
+        {
+            throw new IndexOutOfBoundsException("Error: Must provide a valid index");
+        }
+
+        else
+        {
+            return _backingArray[index];
+        }
+    }
+
+
+    /**
+     * Fetches the index of a given element if it exists
+     *
+     * @param element Element to fetch index for
+     * @return The index value if element exists or -1 if element does not exist
+     */
+    public int indexOf(E element)
+    {
+        int returnValue = DEFAULT_RETURN;
+
+        for (int i = LOW_BOUND; i < _size && returnValue == -1; i++)
+        {
+            if (element == _backingArray[i])
+            {
+                returnValue = i;
+            }
+        }
+
+        return returnValue;
+    }
+
+
+    /**
+     * Determines if the backing array is empty or not
+     *
+     * @return Boolean value based on value of _size variable
+     */
+    public boolean isEmpty()
+    {
+        return _size == LOW_BOUND;
+    }
+
+
+    /**
+     * Removes an element at a given index and shuffles elements accordingly
+     *
+     * @param index Index of element to remove
+     * @return The value of that index
+     * @throws IndexOutOfBoundsException Throws error if index is not within backing array's range
+     */
+    public E remove(int index) throws IndexOutOfBoundsException
+    {
+        E returnIndex = _backingArray[index];
+
+        if (index < LOW_BOUND || index >= _size)
+        {
+            throw new IndexOutOfBoundsException("Error: Must provide a valid index");
+        }
+
+        else
+        {
+            _size -= ITERATION;
+
+            for (int i = LOW_BOUND; i < _size; i++)
+            {
+                set(i, _backingArray[i + ITERATION]);
+            }
+
+            set(_size, null);
+        }
+
+        return returnIndex;
+    }
+
+
+    /**
+     * Replaces the element at the specified index with the given element
+     *
+     * @param index Index of element to replace
+     * @param element New element to replace current element with
+     * @return Previous element at specified index
+     * @throws IndexOutOfBoundsException Throws error if index is not within backing array's range
+     */
+    public E set(int index, E element) throws IndexOutOfBoundsException
+    {
+        E returnIndex = _backingArray[index];
+
+        if (index < LOW_BOUND || index >= _size)
+        {
+            throw new IndexOutOfBoundsException("Error: Must provide a valid index");
+        }
+
+        else
+        {
+            _backingArray[index] = element;
+        }
+
+        return returnIndex;
+    }
+
+
+    /**
+     * Fetches the current size of the ArrayList object
+     *
+     * @return The backing array's current size (_size)
+     */
+    public int size()
+    {
+        return _size;
+    }
+
+
+    // Private Methods
+    /**
+     * Doubles the capacity of the backing array if size reaches capacity
+     */
+    @SuppressWarnings("unchecked")
+    private void grow()
+    {
+        if (_size == _capacity && _size < Integer.MAX_VALUE / 2)
+        {
+            _capacity *= DOUBLE;
+            E[] _tempArray = (E[]) new Object[_capacity];
+
+            for (int i = LOW_BOUND; i < _size; i++)
+            {
+                _tempArray[i] = _backingArray[i];
+            }
+
+            _backingArray = _tempArray;
+        }
+    }
 }
